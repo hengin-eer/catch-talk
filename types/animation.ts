e25f5a41f0aaa5ade04cced9e9642a85f9bafc0c
timdaik <hirokix0608@gmail.com>
2025-12-19 12:27:41 +0900

    add: #43 アニメーションの型定義を分離

---
+++ types/animation.ts
@@ -0,0 +1,18 @@
+export type ActionName =
+  | "catch_before"
+  | "catch_LL"
+  | "catch_LM"
+  | "catch_LR"
+  | "catch_ML"
+  | "catch_MM"
+  | "catch_MR"
+  | "catch_UL"
+  | "catch_UM"
+  | "catch_UR"
+  | "miss"
+  | "normal"
+  | "throw"
+  | "throw_light";
+
+export type PitcherType = "Boy" | "Girl";
+export type CourseType = string;
